4e79b12e83438a8fc80c257b6bf5ea1c2753a57c
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-03-10 13:42:59 +0600

    slider added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,21 @@ $('.counter').counterUp({
     delay: 10,
     time: 2000
 });
+
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+      
