82bb467c40f8af7be932dda9422b496f607bc9cd
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-03-11 10:49:46 +0600

    slider

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// -------------- counter 
 $('.counter').counterUp({
     delay: 10,
     time: 2000
 });
 
-
+// ---------------- slick  slider one 
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -15,8 +16,18 @@ $('.slider-for').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
-    dots: true,
+    dots: false,
     centerMode: true,
     focusOnSelect: true
   });
-      
+// -------------- slick slider two
+
+$('.testimonialSlider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows:true,
+  prevArrow:'<i class="fa-solid fa-arrow-left prevArrow sliderArrow"></i>',
+  nextArrow:'<i class="fa-solid fa-arrow-right nextArrow sliderArrow"></i>'
+});
+
